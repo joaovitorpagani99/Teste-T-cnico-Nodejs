4bf22e78da5ae08875603427857fa551bd765e16
Joao Vitor Pagani <joaovitorpagani7@hotmail.com>
2024-08-30 14:38:49 -0300

    :wrench: subindo configuracao do banco de dados ja ok

--- api-tarefas/src/app.module.ts
+++ api-tarefas/src/app.module.ts
@@ -1,11 +1,32 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TasksModule } from './tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT, 10),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+      extra: {
+        connectionTimeoutMillis: 30000,
+        idleTimeoutMillis: 30000,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      },
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
