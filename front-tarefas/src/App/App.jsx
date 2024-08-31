9e012edcd84e19109c4a1806f06475d0c485c232
Joao Vitor Pagani <joaovitorpagani7@hotmail.com>
2024-08-31 16:05:22 -0300

    :sparkles: feat: adicionando menu

--- front-tarefas/src/App/App.jsx
+++ front-tarefas/src/App/App.jsx
@@ -1,32 +1,30 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Cadastro from '../pages/Cadastro/Cadastro';
 import Login from '../pages/Login/Login';
 import Home from '../pages/Home/Home';
 import Footer from '../components/footer/footer';
+import Menu from "../components/nav/Menu";
 
-import './App.css'
-
+import './App.css';
 
 function App() {
-
   return (
-    <>
+    <Router>
+      <Menu />
       <section className="container">
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/tasks" element={<h1>Tarefas</h1>} />
-            <Route path="/tasks/:id" element={<h1>Tarefa</h1>} />
-            <Route path="cadastro" element={<Cadastro />} />
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<h1>Not Found</h1>} />
-          </Routes>
-        </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tasks" element={<h1>Tarefas</h1>} />
+          <Route path="/tasks/:id" element={<h1>Tarefa</h1>} />
+          <Route path="/cadastro" element={<Cadastro />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<h1>Not Found</h1>} />
+        </Routes>
       </section>
       <Footer />
-    </>
-
-  )
+    </Router>
+  );
 }
 
 export default App;
