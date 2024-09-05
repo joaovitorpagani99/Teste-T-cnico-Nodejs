0da2a89903b0fbe97761f3944ed2d230719ad891
Joao Vitor Pagani <joaovitorpagani7@hotmail.com>
2024-09-05 14:34:25 -0300

    :recycle: refactor: atualizando controller e service

--- front-tarefas/src/pages/Task/ListagemTask/ListagemTask.jsx
+++ front-tarefas/src/pages/Task/ListagemTask/ListagemTask.jsx
@@ -1,24 +1,45 @@
 import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './ListagemTask.css';
 import { getTasks } from '../../../Services/Tasks';
 
 function ListagemTask() {
     const [tasks, setTasks] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         const fetchTasks = async () => {
             try {
-                const data = await getTasks();
-                console.log(data);
+                const token = localStorage.getItem('token');
+                if (!token) {
+                    throw new Error("Token not found in localStorage");
+                }
+
+                const data = await getTasks(token);
                 setTasks(data);
+
             } catch (error) {
+                setError(error.message);
                 console.error("Erro ao buscar tarefas:", error);
+            } finally {
+                setLoading(false);
             }
         };
 
         fetchTasks();
     }, []);
+
+    if (loading) {
+        return <div>Carregando...</div>;
+    }
+
+    if (error) {
+        return <div>Erro: {error}</div>;
+    }
+
+    if (tasks.length === 0) {
+        return <div>Nenhuma tarefa encontrada.</div>;
+    }
 
     return (
         <div className="container mt-5">
