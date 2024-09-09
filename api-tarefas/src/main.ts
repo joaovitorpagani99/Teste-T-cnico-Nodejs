bdbd06d9c91d8ef5f43c09fdf83be507f336d962
Joao Vitor Pagani <joaovitorpagani7@hotmail.com>
2024-09-09 16:52:45 -0300

    :wrench: chore: atualizando o cors

--- api-tarefas/src/main.ts
+++ api-tarefas/src/main.ts
@@ -1,11 +1,15 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors();
+  app.enableCors({
+    origin: "https://teste-tcn-nodejs.vercel.app",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    credentials: true,
+  });
   await app.listen(3000);
 }
 bootstrap();
